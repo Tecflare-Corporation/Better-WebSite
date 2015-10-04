3eff61382cc39e4090d8e653fe69912c6f3a86dc
dodiaraculus <dodiaraculus@outlook.com>
2015-10-04 22:17:01 +0000

    [0.0.4]

--- search.php
+++ search.php
@@ -16,10 +16,29 @@ $results = 0;
                            $results = $results + 1;
                            echo $row->name . "<br>" . $row->author . "<br><a href='http://msite-dodiaraculus.c9.io/preview.php?file=" . $row->name ."'>Read Now</a><br><br>";
                        }
-                                   echo $results . " results found.";
+                                   
                                } else {
-                                   echo "No results found";
-                               }
+                                   $err = $err + 1;
+              
+                              }
+                              echo "<br>";
+                              $result = mysqli_query($conn, "SELECT * FROM Items WHERE `name` LIKE '%".$search_term."%'");
+    if(mysqli_num_rows($result) > 0) {
+
+                       while($row = $result->fetch_object()) {
+                           $results = $results + 1;
+                           echo $row->name . "<br>" . $row->cost . "<br><a href='store.php?addtocart=" . $row->id ."'>Add to Cart</a><br><br>";
+                       }
+                                  
+                               } else {
+                                   $err = $err + 1;
+              
+                              }
+                              echo $results . " results found.<br>";
+                              if ($err == 2) {
+                                   
+                                  echo "No Results Found";
+                              }
 include("theme/footer.php");
 }
 ?>
