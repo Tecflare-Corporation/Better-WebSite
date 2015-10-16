b2fea188352851e7b2201631163b7ac53a81f83e
dodiaraculus <dodiaraculus@outlook.com>
2015-10-16 14:10:46 +0000

    [0.0.5]

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
-echo Install PHP and Updating...
+if [ ! -z "$1" ]; then
+echo "Automated Selected";
 sudo apt-get -qq update
 sudo apt-get -qq install php5
 cd test
@@ -18,3 +19,44 @@ myfunc() {
     
     echo "Success Command Completed Successfully."
     exit 0;
+    
+else 
+
+#!/bin/bash
+
+whiptail --title "Tecflare Multisite" --checklist --separate-output "Choose:" 20 78 15 \
+"PHP" "" on \
+"APACHE" "" off \
+"TEST" "" off 2>results
+
+while read choice
+do
+	case $choice in
+		PHP) sudo apt-get -qq install php5; echo "PHP INSTALLED";
+		;;
+		APACHE) sudo apt-get -qq install apache2; echo "APACHE INSTALLED";
+		;;
+		TEST) cd test
+echo Testing PHP Please Wait
+OUTPUT="$(find ../ -iname "*.php" -exec php -l {} \; )";
+myfunc() {
+    find ../ -iname "*.php" -exec php -l {} \;
+   reqsubstr="$1"
+    shift
+    string="$@"
+    if [ -z "${string##*$reqsubstr*}" ] ;then
+        echo "Error has been detected. System Halting";
+        exit 2;
+    fi
+    }
+    myfunc 'Error' $OUTPUT
+    
+    echo "Success Command Completed Successfully."
+    exit 0;
+    
+		;;
+		*) exit 1;
+		;;
+	esac
+done < results
+fi
