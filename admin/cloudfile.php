2a7f27c8f87fbcfe7a940bdce75b83c48938ec08
dodiaraculus17 <dodiaraculus@outlook.com>
2015-11-13 11:17:13 -0600

    Update cloudfile.php

--- admin/cloudfile.php
+++ admin/cloudfile.php
@@ -9,25 +9,33 @@ $key = md5("Irule17");
 // Encrypt Function
 function mc_encrypt($encrypt, $key){
     $encrypt = serialize($encrypt);
-    $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_DEV_URANDOM);
+    $iva = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_DEV_URANDOM);
     $key = pack('H*', $key);
     $mac = hash_hmac('sha256', $encrypt, substr(bin2hex($key), -32));
-    $passcrypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $encrypt.$mac, MCRYPT_MODE_CBC, $iv);
-    $encoded = base64_encode($passcrypt).'|'.base64_encode($iv);
+    $passcrypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $encrypt.$mac, MCRYPT_MODE_CBC, $iva);
+    $encoded = base64_encode($passcrypt).'|'.base64_encode($iva);
     return $encoded;
 }
 // Decrypt Function
 function mc_decrypt($decrypt, $key){
     $decrypt = explode('|', $decrypt.'|');
     $decoded = base64_decode($decrypt[0]);
-    $iv = base64_decode($decrypt[1]);
-    if(strlen($iv)!==mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)){ return false; }
+    $iva = base64_decode($decrypt[1]);
+    if(strlen($iva)!==mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)) 
+    {
+        return false; 
+        
+    }
     $key = pack('H*', $key);
-    $decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $decoded, MCRYPT_MODE_CBC, $iv));
+    $decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $decoded, MCRYPT_MODE_CBC, $iva));
     $mac = substr($decrypted, -64);
     $decrypted = substr($decrypted, 0, -64);
     $calcmac = hash_hmac('sha256', $decrypted, substr(bin2hex($key), -32));
-    if($calcmac!==$mac){ return false; }
+    if($calcmac!==$mac) 
+    {
+        return false; 
+        
+    }
     $decrypted = unserialize($decrypted);
     return $decrypted;
 }
@@ -56,10 +64,10 @@ $con=mysqli_connect($hostname,$usename,$password,$database);
 $sql="SELECT * FROM Storage";
 
 if ($result=mysqli_query($con,$sql))
-  {
+{
   // Fetch one and one row
   while ($row=mysqli_fetch_row($result))
-    {
+  {
     echo '
           <tr>
         <td><a href="do.php?value=delfile&file=' . $row[0]. '">Delete</a>|<a href="readme.php?value=' . $row[0]. '">Preview</a></td>
@@ -69,6 +77,7 @@ if ($result=mysqli_query($con,$sql))
     }
   // Free result set
   mysqli_free_result($result);
+    
 }
 
 mysqli_close($con);
